52de032aadd5c0deeeff5d3add17ce8a81371050
JavierIH <javier.isabel@bq.com>
2014-12-10 20:24:10 +0100

    Improving target detection

--- programming/src/test/test_boxes.cpp
+++ programming/src/test/test_boxes.cpp
@@ -7,17 +7,21 @@ using namespace cv;
 int main()
 {
 
-    Webcam camera(-1);
+    Webcam camera(1);
 
     while(1){
         Mat image=camera.getFrame();
         imshow("Original",image);
 
-        Mat detection=detectColorRGB("blue",image);
-        imshow("Color azul",detection);
+        Mat detection=detectColorRGB("red",image);
+        //imshow("Color rojo",detection);
 
-        Mat thresh;
-        threshold(detection,thresh,30,255,THRESH_BINARY);
+        Mat1b thresh;
+        threshold(detection,thresh,100,255,THRESH_BINARY);
+
+        thresh=dilation(thresh,-20);
+        thresh=dilation(thresh,20);
+
         imshow("Threshold",thresh);
 
         vector<vector<Point> > contours;
@@ -26,30 +30,39 @@ int main()
 
         vector<vector<Point> > contours_poly( contours.size() );
         vector<Rect> boundRect( contours.size() );
-        vector<Point2f>center( contours.size() );
-        vector<float>radius( contours.size() );
 
-        for( int i = 0; i < contours.size(); i++ )
-           { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-             boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-             minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
-           }
+        for( int i = 0; i < contours.size(); i++ ){
+            approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+            boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+        }
 
-
-        RNG rng(12345);
-
-        /// Draw polygonal contour + bonding rects + circles
         Mat drawing = Mat::zeros( thresh.size(), CV_8UC3 );
-        for( int i = 0; i< contours.size(); i++ )
-           {
-             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-             drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-             rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-             circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
-           }
 
-        imshow( "Contours", drawing );
+        int min_area=10000;
+        int max_area=60000;
 
+        //Threshold de areas
+        if(contours.size()){
+            for( int i = 0; i< contours.size(); i++ ){
+                int area=boundRect[i].height*boundRect[i].width;
+                if(area<min_area||area>max_area){
+                    boundRect.erase(boundRect.begin()+i);
+                }
+            }
+            vector<Point> target;
+
+            for( int i = 0; i< boundRect.size(); i++ ){
+                Scalar color = Scalar(255, 0, 0);
+                rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+                Point center(boundRect[i].x+boundRect[i].width/2,boundRect[i].y+boundRect[i].height/2);
+                target.push_back(center);
+            }
+            report("\n\n\n\n\n\n\n\n\n");
+            report(OK,"Numero de contornos: "+to_string(contours.size()));
+            for( int i = 0; i< target.size(); i++ ) report(INFO,"Centro de contornos "+to_string(target[i]));
+
+            imshow( "Contornos", drawing );
+        }
         if (waitKey(30) == 27) break;
     }
 }
